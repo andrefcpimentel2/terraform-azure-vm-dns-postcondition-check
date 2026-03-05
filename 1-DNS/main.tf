daaf8d7a0f3142c05fab55b9295cc3f29ea2008f
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2026-03-05 16:10:30 +0100

    fix RG name

--- 1-DNS/main.tf
+++ 1-DNS/main.tf
@@ -7,5 +7,5 @@ resource "azurerm_resource_group" "vm_rg" {
 # 2. Azure Private DNS Zone
 resource "azurerm_private_dns_zone" "dns_zone" {
   name                = var.dns_zone_name
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.vm_rg.name
 }
