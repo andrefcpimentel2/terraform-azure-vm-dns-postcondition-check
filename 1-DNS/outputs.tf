a5c0a97fffdcd914b6ba900a3339dc6fcc7ee01d
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2026-03-05 16:09:08 +0100

    no outpus for you, NEXT!

--- 1-DNS/outputs.tf
+++ 1-DNS/outputs.tf
@@ -1,12 +1,3 @@
 # ==========================================
 # 4. OUTPUTS
 # ==========================================
-output "vm_private_ip" {
-  value       = azurerm_network_interface.nic.private_ip_address
-  description = "The private IP address of the newly created VM."
-}
-
-output "vm_fqdn" {
-  value       = "${azurerm_private_dns_a_record.vm_record.name}.${data.azurerm_private_dns_zone.existing_zone.name}"
-  description = "The fully qualified domain name registered in the existing DNS zone."
-}
